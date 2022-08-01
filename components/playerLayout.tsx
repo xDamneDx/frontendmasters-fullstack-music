41debd877f3560d327ea70332a25e27b02dcdac9
xDamneDx <xdamnedx@gmail.com>
2022-08-01 15:00:45 +0300

    Created PlayerLayout component

---
+++ components/playerLayout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Box } from "@chakra-ui/layout";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" left="0" width="250px">
+        Sidebar
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        Player
+      </Box>
+    </Box>
+  );
+};
+
+export default PlayerLayout;
