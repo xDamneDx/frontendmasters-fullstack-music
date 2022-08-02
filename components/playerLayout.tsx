eb3734f5a0236d09e0e7385e0223253cdd4dd3e3
xDamneDx <xdamnedx@gmail.com>
2022-08-02 11:43:14 +0300

    Added Sidebar component

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Box } from "@chakra-ui/layout";
 
+// Components:
+import Sidebar from "./sidebar";
+
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" left="0" width="250px">
-        Sidebar
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         {children}
