d97ba0a0896bd170a4fbb6d5dcb0d76ada8da8ff
xDamneDx <xdamnedx@gmail.com>
2022-08-02 13:18:58 +0300

    Added music menu

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -37,6 +37,19 @@ const navMenu = [
   },
 ];
 
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/",
+  },
+];
+
 const Sidebar = () => {
   return (
     <Box
@@ -70,6 +83,27 @@ const Sidebar = () => {
             ))}
           </List>
         </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {musicMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider color="gray.800" />
       </Box>
     </Box>
   );
