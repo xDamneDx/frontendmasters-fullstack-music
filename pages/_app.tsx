d3a9bfd6922e94aa4469544848d6fa2d9642b83e
xDamneDx <xdamnedx@gmail.com>
2022-08-05 15:07:09 +0300

    Added conditional layout changing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,9 +36,13 @@ const theme = extendTheme({
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <PlayerLayout>
+      {Component.authPage ? (
         <Component {...pageProps} />
-      </PlayerLayout>
+      ) : (
+        <PlayerLayout>
+          <Component {...pageProps} />
+        </PlayerLayout>
+      )}
     </ChakraProvider>
   );
 };
