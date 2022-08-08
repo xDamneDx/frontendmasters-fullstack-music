df0df1335c7ea895ea798589441cb19961052acf
xDamneDx <xdamnedx@gmail.com>
2022-08-08 21:54:38 +0300

    Added usePlaylist hook

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -19,6 +19,9 @@ import {
   MdFavorite,
 } from "react-icons/md";
 
+// Custom hooks:
+import { usePlaylist } from "../lib/hooks";
+
 const navMenu = [
   {
     name: "Home",
@@ -50,9 +53,9 @@ const musicMenu = [
   },
 ];
 
-const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
-
 const Sidebar = () => {
+  const { playlists, isLoading, isError } = usePlaylist();
+
   return (
     <Box
       width="100%"
@@ -109,10 +112,10 @@ const Sidebar = () => {
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
             {playlists.map((playlist) => (
-              <ListItem paddingX="20px" key={playlist}>
+              <ListItem paddingX="20px" key={playlist.id}>
                 <LinkBox>
                   <NextLink href="/" passHref>
-                    <LinkOverlay>{playlist}</LinkOverlay>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
