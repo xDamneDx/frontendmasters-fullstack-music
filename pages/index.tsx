d4c413a6fcfa6cb98e35677fdb68f0ff63c53963
xDamneDx <xdamnedx@gmail.com>
2022-08-09 17:18:40 +0300

    Added GradientLayout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+// Components:
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      color="red"
+      subtitle="profile"
+      title="Some Name"
+      description="10 public playlists"
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+      roundImage
+    >
+      <div>Home Page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
