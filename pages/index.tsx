93ad15dde56983242f708b3feba6c3bd40e4e8e8
xDamneDx <xdamnedx@gmail.com>
2022-08-09 17:32:32 +0300

    Added getServerSideProps w/ artists data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+import prisma from "../lib/prisma";
 
 // Components:
 import GradientLayout from "../components/gradientLayout";
 
-const Home = () => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       color="red"
@@ -13,9 +16,40 @@ const Home = () => {
       image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
       roundImage
     >
-      <div>Home Page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%" key={artist.name}>
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
