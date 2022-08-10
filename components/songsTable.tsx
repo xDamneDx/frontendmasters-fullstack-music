1b1b12f5418dba882b1c14d93e3f71af448cf799
xDamneDx <xdamnedx@gmail.com>
2022-08-10 12:15:49 +0300

    Created SongsTable component

---
+++ components/songsTable.tsx
@@ -0,0 +1,55 @@
+import { Box } from "@chakra-ui/layout";
+import { Table, Thead, Tr, Td, Th, Tbody, IconButton } from "@chakra-ui/react";
+import { BsFillPlayFill } from "react-icons/bs";
+import { AiOutlineClockCircle } from "react-icons/ai";
+
+const SongsTable = ({ songs }) => {
+  return (
+    <Box bg="transparent" color="white">
+      <Box padding="10px" marginBottom="20px">
+        <Box marginBottom="30px">
+          <IconButton
+            icon={<BsFillPlayFill fontSize="30px" />}
+            aria-label="play"
+            colorScheme="green"
+            size="lg"
+            isRound
+          />
+        </Box>
+        <Table variant="unstyled">
+          <Thead borderBottom="1px solid" borderColor="rgba(255,255,255,0.2)">
+            <Tr>
+              <Th>#</Th>
+              <Th>Title</Th>
+              <Th>Date Added</Th>
+              <Th>
+                <AiOutlineClockCircle />
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {songs.map((song, i) => (
+              <Tr
+                sx={{
+                  transition: "all .3",
+                  "&:hover": {
+                    bg: "rgba(255,255,255,0.1)",
+                  },
+                }}
+                key={song.id}
+                cursor="cursor"
+              >
+                <Td>{i + 1}</Td>
+                <Td>{song.name}</Td>
+                <Td>{song.createdAt.toString()}</Td>
+                <Td>{song.duration}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
+};
+
+export default SongsTable;
