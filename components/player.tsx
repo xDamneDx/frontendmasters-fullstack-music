cc78fa3fed3e82c705ff74e656ad2b09865a0229
xDamneDx <xdamnedx@gmail.com>
2022-08-11 11:49:48 +0300

    Added player controls & seek bar

--- components/player.tsx
+++ components/player.tsx
@@ -23,6 +23,82 @@ import {
 import { useStoreActions } from "easy-peasy";
 
 const Player = () => {
-  return <Box>Player</Box>;
+  return (
+    <Box>
+      <Box>{/* <ReactHowler /> */}</Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="previous"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="play"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePlayCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="pause"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="xs">0:00</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={["min", "max"]}
+              step={0.1}
+              min={0}
+              max={100}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs">9:59</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
 };
 export default Player;
